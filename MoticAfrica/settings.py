411bfaa2494662c15efc6384d9cab3f3dda4b4f7
Murangiri772 <YOUR_GITHUB_EMAIL>
2026-09-22 12:41:25 -0700

    Fix static file serving with WhiteNoise

--- MoticAfrica/settings.py
+++ MoticAfrica/settings.py
@@ -55,9 +55,9 @@ INSTALLED_APPS = [
 # ============================================================
 # MIDDLEWARE
 # ============================================================
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 
     "django.contrib.sessions.middleware.SessionMiddleware",
 
