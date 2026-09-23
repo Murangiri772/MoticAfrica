630adfd389ae85228573f7b6d950907d10049d51
Murangiri772 <YOUR_GITHUB_EMAIL>
2026-09-23 03:34:44 -0700

    Remove booking and payment features

--- moticapp/urls.py
+++ moticapp/urls.py
@@ -1,75 +1,14 @@
-
-
 from django.urls import path
-
-
 from moticapp import views
 
 urlpatterns = [
-
     path('', views.index, name='index'),
-    path('starter/', views.starter, name='starter'),
 
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
 
-
-
-    path(
-        'team/register/',
-        views.team_registration,
-        name='team_registration'
-    ),
-
-     path(
-       'team/success/<int:team_id>/',
-        views.team_success,
-         name='team_success'
-      ),
-
-     path(
-      'team/ticket/<int:team_id>/',
-      views.team_ticket,
-      name='team_ticket'
-     ),
-      path('teams/', views.teams, name='teams'),
-       path('battle/', views.battle, name='battle'),
-      path('gallery/', views.gallery, name='gallery'),
-      path('tickets/', views.tickets, name='tickets'),
-      path('tickets/', views.tickets, name='tickets'),
-
-    path(
-       'tickets/book/',
-      views.ticket_booking,
-      name='ticket_booking'
-     ),
-
-      path(
-        'tickets/success/<int:booking_id>/',
-          views.ticket_success,
-          name='ticket_success'
-      ),
-     path(
-      'tickets/qr/<int:booking_id>/',
-      views.ticket_qr,
-      name='ticket_qr'
-      ),
-path(
-    'team/payment/<int:team_id>/',
-    views.team_payment,
-    name='team_payment'
-),
-path(
-    'tickets/payment/<int:ticket_id>/',
-    views.ticket_payment,
-    name='ticket_payment'
-),
-
-
-
-
-
-
-
+    path('teams/', views.teams, name='teams'),
+    path('battle/', views.battle, name='battle'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('tickets/', views.tickets, name='tickets'),
 ]
-
